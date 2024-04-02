4769a0b2e1e551472402e54e833b1662cbdfa4b7
Francisco José Coto Pacheco <fcoto@uoc.edu>
2024-04-03 01:17:19 +0200

    P1

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,7 +10,42 @@
  * Write any other JavaScript below
  */
 
-+( function() {
++(function () {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
-} )();
+})();
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector('.header__nav__buttons__menu');
+  const closeButton = document.querySelector('.header__nav__buttons__close');
+  const menu = document.querySelector('.header__nav__ul');
+
+  menuToggle.addEventListener('click', function () {
+    menu.classList.toggle('header__nav__ul--open');
+    menuToggle.style.display = 'none';
+    closeButton.style.display = 'block';
+  });
+
+  closeButton.addEventListener('click', function () {
+    menu.classList.remove('header__nav__ul--open');
+    menuToggle.style.display = 'block';
+    closeButton.style.display = 'none';
+  });
+});
+
+document.addEventListener("scroll", function () {
+  changeStylesHeader();
+});
+
+function changeStylesHeader() {
+  var header = document.querySelector('.header');
+  var scrollPosition = window.scrollY;
+
+  var threshold = 240;
+
+  if (scrollPosition > threshold) {
+    header.classList.add('header--scrolled');
+  } else {
+    header.classList.remove('header--scrolled');
+  }
+}
